e33ceaab0eeaf3a8389d272fde33b6cdd473f8e0
Renan Fiuja <renanfiuja@hotmail.com>
2024-09-19 15:34:39 -0300

    Adicionado funcao PHP no formulario para enviar emails

---
+++ envia.php
@@ -0,0 +1,20 @@
+<?php
+
+    $nome     = addcslashes($_POST['nome']);
+    $email    = addcslashes($_POST['email']);
+    $telefone = addcslashes($_POST['telefone']);
+    $mensagem = addcslashes($_POST['mensagem']);
+
+    $para = 'renanfiujah@gmail.com';
+    $assunto =  'NOVO CONTATO !';
+    $corpo = "Nome: ".$nome."\n"."Email: ".$email."\n"."Telefone: "."\n"."Mensagem: ".$mensagem;
+    $cabecalho = "From: portifolio@fiuja.com"."\n"."Reply-to ".$email."\n"."X-Mailer:PHP/".phpversion();
+
+    if(mail($para,$assunto,$corpo,$cabecalho)){
+        echo("Mensagem enviada com sucesso!");
+    }else{
+        echo("Houve um problema ao enviar a mensagem, verifique os campos e tente novamente !")
+    }
+
+
+?>
