04820e07c37188b2362f841371ad553ceb352b77
Renan Fiuja <renanfiuja@hotmail.com>
2024-09-23 07:26:22 -0300

    portifolio alterado, links adicionado aos botoes

---
+++ main.js
@@ -0,0 +1,20 @@
+// Seleciona todas as imagens expansíveis
+//const images = document.querySelectorAll('.img-port');
+//
+//// Função para expandir ou recolher a imagem
+//images.forEach(image => {
+//    image.addEventListener('click', function() {
+//        // Verifica se a imagem já está expandida
+//        const isExpanded = image.classList.contains('expanded');
+//
+//        // Remove a classe 'expanded' de todas as imagens
+//        images.forEach(img => img.classList.remove('expanded'));
+//
+//        // Se não estava expandida, expande a imagem clicada
+//        if (!isExpanded) {
+//            image.classList.add('expanded');
+//        }
+//    });
+//});
+//
+
